d938ab24ccd430e04b95536e64332680b7bd732c
Tolga Durak <tolgadurak.dev@gmail.com>
2024-12-30 16:56:56 +0300

    Scaffold loan-api module

---
+++ loan-api/src/test/java/io/github/tolgadurak/creditmodulechallenge/loanapi/LoanApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.tolgadurak.creditmodulechallenge.loanapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
